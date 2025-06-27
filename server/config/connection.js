9342b07def4aec5d4a6d1012821a9367b1839a05
Sonam Sherpa <sherpa.sjs@gmail.com>
2025-06-27 14:06:02 -0700

    change db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/roster-hub-v2",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/roster-hub",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
